1e4c95e1dc821434b647826533a2dfe215619553
AlicePlentz <alicecplentz@gmail.com>
2024-02-01 19:04:40 -0300

    V2 operadores lógicos

---
+++ tipos de operadores/op_associacao.py
@@ -0,0 +1,10 @@
+curso = "Curso de Python"
+frutas = ['uva', 'limão']
+saques = [1500, 200]
+
+print('laranja' in frutas)
+print('curso de python' in curso)
+print('Curso de Python' not in curso)
+print(1500 and 300 in saques)
+print(1500 or 300 in saques)
+
---
+++ tipos de operadores/op_identidade.py
@@ -0,0 +1,17 @@
+curso = 'Curso de Pyhton'
+nome_curso = curso 
+
+print(curso is nome_curso)
+print(curso is not nome_curso)
+
+saldo = 1000
+limite = 500
+print(saldo is limite)
+print(saldo is not limite)
+
+saldo = 1000
+limite = 1000
+print(saldo is limite)
+print(saldo is not limite)
+
+#op de verificação, se ocupa o mesmo espaço de memória
